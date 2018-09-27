ef9afdf2236e9e031ae117bc248526bc70c230c0
Zack Kollar (SeedyROM) <rallokkcaz@gmail.com>
2018-09-27 02:59:39 -0700

    Fixed typos and 1st person usage in the comments

--- src/broilerplate/logger.cr
+++ src/broilerplate/logger.cr
@@ -1,16 +1,17 @@
 require "colorize"
 
+# Used to make our standard output look nice
 module Broilerplate::Logger
   # Set the default spacing value
   SPACING = "   "
-  # The contstant our define_levels macro will use to generate our methods
+  # A constant used to help the `define_levels` macro
   LEVELS = [
     {:name => :success, :color => :green, :symbol => "✓"},
     {:name => :working, :color => :yellow, :symbol => "🏋️"},
     {:name => :error, :color => :red, :symbol => "☠️"},
   ]
 
-  # Define loggin methods based on the constant LEVELS
+  # Define logging methods from the LEVELS constant
   macro define_levels
     {% for level in LEVELS %}
       def self.{{level[:name].id}}(msg)
@@ -19,6 +20,5 @@ module Broilerplate::Logger
     {% end %}
   end
 
-  # Execute the Loggin helper macro
   define_levels
 end
